0c76da195e458caa0060e8486151c4d2fd338ea1
Cormac <cormacdoyle802@gmail.com>
2020-08-08 18:54:37 -0400

    Finishing Commenting

--- problems.py
+++ problems.py
@@ -16,7 +16,10 @@ def mystery(lis, i=0):
         lis[index],lis[x] = lis[x],lis[index]
 
     print(lis, i)
-
+'''
+This function takes a non-negative integer as its paramater and returns the number of
+pairs of numbers within the integer that sum to 10.
+'''
 def findPairs(n, count=0, y=-1, i = 0):
     try: # try except statement ensures that input is correct data type
         if n >= 0: #checks if inputted integer is 0 or a positive integer
@@ -48,7 +51,11 @@ def findPairs(n, count=0, y=-1, i = 0):
         return "Please input an integer"
     except TypeError:
         return "Please input an integer"
-    
+
+'''
+This function accepts a list of lists called nestedLis as its parameter and returns
+a single list containing all of the values in nestedLis.
+'''
 def mergeList(nestedLis):
     if isinstance(nestedLis, list) == True: #ensures that input is a list
         if len(nestedLis) == 0: #base case
@@ -60,7 +67,10 @@ def mergeList(nestedLis):
             return nestedLis[:1] + mergeList(nestedLis[1:])
     else: #if input is not a list raises this error message
         return "Please enter a list or nested list"
-
+'''
+This function accepts a list of lists called nestedLis as its parameter and returns
+the smallest value in nestedLis.
+'''
 def recMin(nestedLis):
     if isinstance(nestedLis, list) == True: #ensures that input is a list
         nestedLis = mergeList(nestedLis) #cleans nested list to make it a list
@@ -76,7 +86,10 @@ def recMin(nestedLis):
             return nestedLis[0]
     else: #if input is not a list returns this error message
         return "Please enter a list or nested list"
-    
+'''
+This function takes a single integer n as its parameter and calculates the sum of
+every other integer between 0 and n.
+'''
 def addNext(n, count = 0):
     if isinstance(n, int): #checks to see if input is an integer
         if n>0: #recursive case 
@@ -86,7 +99,10 @@ def addNext(n, count = 0):
             return count
     else:#if input is not an integer returns this error message
         return "Please enter an integer"
-
+'''
+This function accepts a list of integers as its parameter and returns a copy of
+the list in which neighbouring elements have been swapped.
+'''
 def swapElements(listInput, i = 0):
     if isinstance(listInput, list) == True: #checks if input is a list
         length  = len(listInput) - 1
@@ -98,19 +114,28 @@ def swapElements(listInput, i = 0):
             return listInput
     else: #if input is not a list returns this error message
         return "Please enter a list"
-
+'''
+This function takes an integer as its parameter and returns a list in which each
+element of that number is an individual list element.
+'''
 def integerToList(n): #helper function changes integer into a list for easier manipulation (allowed by Professor Allison)
     n = str(n) #turns int to string
     newlist = []
     for i in n:
         newlist.append(i)
     return newlist # returns list with each number in the integer as a list element
-
-def listToInteger(n): #helper function changes list into an integer for easier manipulation (allowed by Professor Allison)
+'''
+This function takes a list of integers as its parameter and returns an integer
+of those values combined.
+'''
+def listToInteger(alist): #helper function changes list into an integer for easier manipulation (allowed by Professor Allison)
     string = ""
-    string = string.join(n)
+    string = string.join(alist)
     return int(string) #returns integer
-
+'''
+This function accepts a positive integer as its parameter and returns the integer
+backwards.
+'''
 def funkyNums(n, i=0):
     if isinstance(n, int) == True:#checks to make sure input is an integer
         n = integerToList(n) # converts integer into list
@@ -124,7 +149,11 @@ def funkyNums(n, i=0):
             return listToInteger(n)
     else:#if input is not an integer returns this error message
         return "Please enter an integer"
-
+'''
+This function returns the minimum number of stamps that would be required in order
+to pay the postage cost, the function accepts a postage cost and a list of integers
+representing the stamp denominations as its parameters.
+'''
 def calcStamp(cost, stampList, resultList = None, i=0 ):
     if isinstance(cost, int) == True and isinstance(stampList, list) == True: #checks if cost is integer and stamp list is list
         if resultList is None: #clears resultList from last function call
@@ -133,7 +162,7 @@ def calcStamp(cost, stampList, resultList = None, i=0 ):
         if i < length+1: #recursive case
             if stampList[length-i] <= cost: #checks for closest value less than or equal to cost
                 resultList.append(stampList[length-i]) #adds highest value to result list
-                cost-= stampList[length-i] subtracts found value #changes cost variable to outstanding cost balance
+                cost-= stampList[length-i]  #changes cost variable to outstanding cost balance
                 i=0 #resets index value
                 return calcStamp(cost, stampList, resultList, i)
             else:
@@ -143,7 +172,10 @@ def calcStamp(cost, stampList, resultList = None, i=0 ):
             return len(resultList) 
     else: #if cost is not integer and stamp list is not a list return this error message
         return "Invalid, please enter an integer followed by a list"
-
+'''
+Test function shows what should be returned for a function for a given output and
+what the function actually outputs.
+'''
 def unitTest():
     print("Testing Find Pairs:")
     print("For input", 7645238, ",should return 3,", findPairs(7645238))
@@ -193,8 +225,7 @@ def unitTest():
     print("For input", 19, [1, 2, 5, 12, 14, 18], ",should return 1,", calcStamp(18, [1, 2, 5, 12, 14, 18]))
     print("For input", [1, 2, 5, 12, 14, 18], 19,",should return Invalid, please enter an integer followed by a list", calcStamp([1, 2, 5, 12, 14, 18], 19))
     print("For input", 'This List', 0, ",should return Invalid, please enter an integer followed by a list,", calcStamp('This list', 0))
-
-    mystery([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+    
 def main():
     unitTest()
 
